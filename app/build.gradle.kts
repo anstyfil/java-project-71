b9a2abe1f0dafe12172a43512a30fcd6bf4059cf
anstyfil <sloikasvishney@gmail.com>
2026-03-31 12:05:47 +0300

    compile and build the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("java")
+    id("application")
     id("com.github.ben-manes.versions") version "0.52.0"
 }
 
@@ -13,6 +13,10 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass = "hexlet.code.App"
 }
 
 tasks.test {
