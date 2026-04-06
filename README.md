9d8dc2bb7d8bdb4e268abdd9e22090a4462a4595
anstyfil <sloikasvishney@gmail.com>
2026-04-06 19:51:21 +0300

    Implement flat JSON comparison logic

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/anstyfil/java-project-71/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/anstyfil/java-project-71/actions)
+https://asciinema.org/a/0CaKxmjZ0XSzVNqz
+
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -2,6 +2,9 @@ package hexlet.code;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -22,6 +25,59 @@ public class Differ {
     public static String generate(String filePath1, String filePath2) throws Exception {
         Map<String, Object> file1 = getData(filePath1);
         Map<String, Object> file2 = getData(filePath2);
-        return file1 + System.lineSeparator() + file2;
+
+        List<String> keys = new ArrayList<>(file1.keySet());
+
+        StringBuilder result = new StringBuilder();
+        result.append("{\n");
+
+        for (String key : file2.keySet()) {
+            if (!keys.contains(key)) {
+                keys.add(key);
+            }
+        }
+        Collections.sort(keys);
+
+        for (String key : keys) {
+            boolean inFirst = file1.containsKey(key);
+            boolean inSecond = file2.containsKey(key);
+
+            Object value1 = file1.get(key);
+            Object value2 = file2.get(key);
+
+            if (inFirst && !inSecond) {
+                result.append("- ")
+                        .append(key)
+                        .append(": ")
+                        .append(value1)
+                        .append("\n");
+            } else if (!inFirst && inSecond) {
+                result.append("+ ")
+                        .append(key)
+                        .append(": ")
+                        .append(value2)
+                        .append("\n");
+            } else if (value1.equals(value2)){
+                result.append("  ")
+                        .append(key)
+                        .append(": ")
+                        .append(value1)
+                        .append("\n");
+            } else {
+                result.append("- ")
+                        .append(key)
+                        .append(": ")
+                        .append(value1)
+                        .append("\n");
+
+                result.append("+ ")
+                        .append(key)
+                        .append(": ")
+                        .append(value2)
+                        .append("\n");
+            }
+        }
+        result.append("}");
+        return result.toString();
     }
 }
