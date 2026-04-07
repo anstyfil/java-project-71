8b79b5d5fd740fbbdf04043cbbd61b89a1ab5963
anstyfil <anstyfil@gmail.com>
2026-04-07 22:40:12 +0300

    Add checkstyle, GitHub Actions and SonarQube

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("application")
     id("com.github.ben-manes.versions") version "0.52.0"
+    id("checkstyle")
+    id("org.sonarqube") version "7.2.3.7755"
 }
 
 group = "hexlet.code"
@@ -15,11 +17,26 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.0.1")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
 }
 
 application {
     mainClass = "hexlet.code.App"
 }
+
+checkstyle {
+    toolVersion = "10.12.4"
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "anstyfil_java-project-71")
+        property("sonar.projectName", "java-project-71")
+        property("sonar.organization", "anstyfil")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
+
 
 tasks.test {
     useJUnitPlatform()
